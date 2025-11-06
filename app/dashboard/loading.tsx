9a4e16e3cd60bde2c4745e1bc6a419e1998a1076
Arjun Sharma <whocaresaboutarjun@gmail.com>
2025-11-06 07:40:17 +0530

    man

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -7,7 +7,7 @@ export default function DashboardPageLoader() {
   return (
     <PageWrapper breadcrumbs={[{ label: "Dashboard", href: "/dashboard" }]} title="Dashboard">
         <div className="flex items-center w-full justify-between gap-2">
-            <p className="font-semibold text-xl w-full md:text-2xl">Your Notebooks</p>
+            <p className="font-semibold text-xl w-full md:text-2xl">Your Notebookss</p>
             <Skeleton className="h-8 w-8 md:w-16 lg:w-24" />
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-2">
