50acb6266f313bc6f1eba5f01d85279df3122da1
Arjun Sharma <whocaresaboutarjun@gmail.com>
2025-11-10 00:05:19 +0530

    wrap useQueryState in Suspense [build error]

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,16 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { Suspense } from "react";
 
 export default function DashboardLayout({children}: {children: React.ReactNode}) {
     return (
-        <SidebarProvider>
-            <AppSidebar />
-            <main className="flex-1">
-                {children}
-            </main>
-       </SidebarProvider>
+        <Suspense>
+            <SidebarProvider>
+                <AppSidebar />
+                <main className="flex-1">
+                    {children}
+                </main>
+            </SidebarProvider>
+        </Suspense>
     )
 }
