8073019241d6e13a8b2b4715dcc1f81873f33ee5
Arjun Sharma <whocaresaboutarjun@gmail.com>
2025-11-09 23:45:16 +0530

    add nuqs and make the search change the url params

--- components/search-form.tsx
+++ components/search-form.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Search } from "lucide-react"
 
 import { Label } from "@/components/ui/label"
@@ -7,7 +9,12 @@ import {
   SidebarInput,
 } from "@/components/ui/sidebar"
 
+import { useQueryState } from "nuqs"
+
 export function SearchForm({ ...props }: React.ComponentProps<"form">) {
+
+  const [ query, setQuery ] = useQueryState("search", { defaultValue: "" });
+
   return (
     <form {...props}>
       <SidebarGroup className="py-0">
@@ -19,6 +26,8 @@ export function SearchForm({ ...props }: React.ComponentProps<"form">) {
             id="search"
             placeholder="Search the notes..."
             className="pl-8"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
           />
           <Search className="pointer-events-none absolute top-1/2 left-2 size-4 -translate-y-1/2 opacity-50 select-none" />
         </SidebarGroupContent>
